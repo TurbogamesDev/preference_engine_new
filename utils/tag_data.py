c182e28d23494650c78ef74c1e50e5f4a5f17e68
TurbogamesDev <turbogames.mail@gmail.com>
2026-07-05 09:24:44 +0530

    Change identifier of tag to tag_id

--- utils/tag_data.py
+++ utils/tag_data.py
@@ -1,5 +1,7 @@
 class TagData:
-    def __init__(self, tag_name: str, tag_rank: int) -> None:
+    def __init__(self, tag_id: int, tag_name: str, tag_rank: int) -> None:
+        self.tag_id: int = tag_id
+
         self.tag_name: str = tag_name
 
         self.tag_weight: float = tag_rank / 100.0
