e04cffaba6bed12f0ea4cb5aa85781c023c11979
TurbogamesDev <turbogames.mail@gmail.com>
2026-07-05 13:27:13 +0530

    Archive log files

--- helpers/tag_scores_calculator.py
+++ helpers/tag_scores_calculator.py
@@ -5,8 +5,6 @@ def calculate_tag_scores(started_entries_dict: dict[int, Entry]) -> dict[int, Ta
     tag_scores: dict[int, TagScore] = {}
 
     for entry in list(started_entries_dict.values()):
-        print(f"[{entry.status}] {entry.media.title} ({entry.progress}/{entry.media.total_episodes})")
-
         for tag_data in entry.media.tags:
             if not tag_scores.get(tag_data.tag_id):
                 tag_scores[tag_data.tag_id] = TagScore(tag_data.tag_id, tag_data.tag_name)
